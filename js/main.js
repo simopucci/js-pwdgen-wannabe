e10f215f4271b0a94787d9fbb26175b7e058c463
simopucci <simo.pucci7@gmail.com>
2024-01-08 17:24:36 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const passGenerated = document.getElementById(pwgen);
+
+// Chiedo il nome
+const yourName = prompt('Qual è il tuo nome?');
+console.log(yourName);
+
+// Chiedo il cognome
+const yourSurname = prompt('Qual è il tuo cognome?');
+console.log(yourSurname);
+
+// Chiedo il colore preferito
+const yourColor = prompt('Qual è il tuo colore preferito?');
+console.log(yourColor);
+
+// Calcolo le 3 informazioni e le salvo
+const yourPassword = yourName + yourSurname + yourColor + 24;
+console.log(yourPassword);
+
+// Stampo la variabile 
+passGenerated.innerText = "La password che abbiamo generato per te è: " + yourPassword;
---
+++ readme.md
@@ -0,0 +1,16 @@
+## TRACCIA 
+
+```
+Chiedi all'utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito23
+```
+
+### SVOLGIMENTO
+
+- Chiedo il nome
+- Chiedo il cognome 
+- Chiedo il colore preferito
+- Calcolo le 3 informazioni e le salvo in una variabile
+- Stampo la variabile con le informazioni salvate
